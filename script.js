7d6ee5a9e53730e5eaa3747e93e473c9bec76217
alimnika <alimnika87@gmail.com>
2021-09-29 23:23:12 -0500

    UpTo #10 Done

--- script.js
+++ script.js
@@ -57,3 +57,120 @@ const isGreaterThanTen = num =>{
 isGreaterThanTen(11)
 
 // EX #6
+// getLengthOfTwoWords
+// Write a function called "getLengthOfTwoWords".
+// Given 2 words, "getLengthOfTwoWords" returns the sum of their lengths.
+
+// var output = getLengthOfTwoWords('some', 'words');
+// console.log(output); // --> 9
+
+function getLengthOfTwoWords(word1, word2){
+    return word1.length+word2.length;
+}
+getLengthOfTwoWords('some', 'words')
+// Second Option
+const  getLengthOfTwoWords =(word1, word2) =>{
+    let count=0;
+      for (let i=0; i<word1.length; i++){
+        count+=1
+      }
+      for(let x=0; x<word2.length; x++){
+        count +=1
+      }
+      return count
+  }
+  getLengthOfTwoWords('some', 'words')
+
+ 
+// EX #7
+// isLessThan30
+// Write a function called "isLessThan30".
+// Given a number, "isLessThan30" returns whether the given number is less than 30.
+
+// var output = isLessThan30(9);
+// console.log(output); // --> true
+
+function isLessThan30(num){
+    if(num < 30){
+      return true;
+    }else{
+      return false;
+    }
+  }
+  isLessThan30(9)
+//   Using ES6
+const isLessThan30 = num =>{
+    let result = num < 30 ? true : false
+    return result;
+}
+isLessThan30(9)
+
+// Ex #8
+// equalsTen
+// Write a function called "equalsTen".
+// Given a number, "equalsTen" returns whether or not the given number is 10.
+
+// var output = equalsTen(9);
+// console.log(output); // --> false
+
+function equalsTen(num){
+    if(num >10){
+        return true;
+    }else{
+        return false;
+    }
+}
+equalsTen(9)
+//  Using ES6
+const equalsTen = num =>{
+    let result = num >10 ? true : false;
+    return result
+  }
+  equalsTen(9)
+
+//   EX #9
+// isLessThan
+// Write a function called "isLessThan".
+// Given 2 numbers, "isLessThan" returns whether num2 is less than num1.
+
+// var output = isLessThan(9, 4);
+// console.log(output); // --> true
+
+function isLessThan(num1, num2){
+    if(num2 < num1){
+        return true;
+    }else{
+        return false;
+    }
+}
+isLessThan(9, 4);
+
+// Using ES 6
+const isLessThan = (num1, num2) => {
+  let result = num2<num1 ? true : false;
+  return result
+}
+isLessThan(9, 4);
+
+// EX #10
+// isGreaterThan
+// Write a function called "isGreaterThan".
+// Given 2 numbers, "isGreaterThan" returns whether num2 is greater than num1.
+
+// var output = isGreaterThan(11, 10);
+// console.log(output); // --> false
+
+function isGreaterThan (num1, num2){
+    if(num2 > num1){
+        return true;
+    }else{
+        return false;
+    }
+}
+isGreaterThan(11, 10);
+// Using ES6
+const isGreaterThan = (num1, num2) =>{
+  let result = num2 > num1 ? true : false;
+  return result;
+}
+isGreaterThan(11, 10)
