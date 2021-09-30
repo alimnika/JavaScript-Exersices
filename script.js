14233d4dcea7dc5f283b4ea51d32a3e42c978364
alimnika <alimnika87@gmail.com>
2021-09-29 20:40:29 -0500

    Up To Ex#6 Done

--- script.js
+++ script.js
@@ -1,6 +1,59 @@
-// EX 1
+// EX #1
 // print
 // Your function will be given an input parameter incoming. Please console.log this value.
 // `print('test') // test`
 let  print="test"; 
-onsole.log(print)
+console.log(print)
+
+// EX #2
+// sayHello
+// Create a function sayHello(). Within it, console.log string "Hello Seytech!"
+// **sayHello()**
+
+const sayHello = () =>{
+    return "Hello World!"
+}
+sayHello()
+
+// EX #3
+// getFullName
+// Write a function called "getFullName".
+// Given a first and a last name, "getFullName" returns a single string with the given first and last names separated by a single space.
+
+// var output = getFullName('Joe', 'Smith');
+// console.log(output); // --> 'Joe Smith'
+
+const getFullName = (firstName, lastName) =>{
+    return `${firstName} ${lastName}`;
+}
+getFullName('Joe','Smith')
+
+// EX #4
+// getLengthOfWord
+// Write a function called "getLengthOfWord".
+// Given a word, "getLengthOfWord" returns the length of the given word.
+
+// var output = getLengthOfWord('some');
+// console.log(output); // --> 4
+
+const getLengthOfWord = word => {
+    return word.length;
+}
+getLengthOfWord('some')
+
+// EX #5
+// isGreaterThanTen
+// Write a function called "isGreaterThanTen".
+// Given a number, "isGreaterThanTen" returns whether it is greater than 10.
+
+// var output = isGreaterThanTen(11);
+// console.log(output); // --> true
+ 
+const isGreaterThanTen = num =>{
+    let result = num >10 ? `true`:`false`;
+    return result;
+    
+}
+isGreaterThanTen(11)
+
+// EX #6
